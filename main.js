fc6713cbd06e1a5f2583ee9ed17f4c0b21360065
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-10-27 15:48:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import { Gantt } from "@bryntum/gantt";
+import "@bryntum/gantt/gantt.stockholm.css";
+
+const gantt = new Gantt({
+  appendTo: document.body,
+
+  project: {
+    taskStore: {
+      autoTree: true,
+      transformFlatData: true,
+      listeners: {
+        change: function(event) {
+        console.log(event);
+        }
+      },
+    },
+    transport: {
+      load: {
+        url: "http://localhost:1337/data",
+      },
+      sync: {
+        url: "http://localhost:1337/api",
+      },
+    },
+    autoLoad: true,
+    autoSync: true,
+    validateResponse: true,
+  },
+
+  columns: [{ type: "name", width: 250, text: "Tasks" }],
+});
